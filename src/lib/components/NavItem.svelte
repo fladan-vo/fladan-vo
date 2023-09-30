9eafddee3e76a4f81f4d5ecf6b5ba753dd2f305c
adam <adamliliemark@gmail.com>
2023-09-30 14:20:37 +0200

    one more

--- src/lib/components/NavItem.svelte
+++ src/lib/components/NavItem.svelte
@@ -1,25 +1,23 @@
 <script>
-	import { currentPage, isMenuOpen } from '$lib/assets/js/store'
+	import { currentPage, isMenuOpen } from '$lib/assets/js/store';
+	import { base } from '$app/paths';
+	export let href;
 
-	export let href
-
-	$: isCurrentPage = $currentPage.startsWith(href)
+	$: isCurrentPage = $currentPage.startsWith(href);
 
 	const maybeCloseMenu = () => {
 		if (href != $currentPage) {
-			isMenuOpen.set(false)
+			isMenuOpen.set(false);
 		}
-	}
+	};
 </script>
-
 
 <li>
 	<a
-		href={href}
+		href="{base}/{href}"
 		on:click={maybeCloseMenu}
 		class:active={isCurrentPage}
-		aria-current={isCurrentPage ? 'page' : false}
-	>
+		aria-current={isCurrentPage ? 'page' : false}>
 		<slot />
 	</a>
 </li>
