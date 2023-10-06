3bba0ed1a83859559477aeb772d1634f499685f9
adam <adamliliemark@gmail.com>
2023-10-06 16:17:39 +0200

    new again

--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -8,7 +8,7 @@
 <div class="mx-auto max-w-page">
 	<h1 class="text-4xl font-bold mt-8">Woops!</h1>
 	<p class="py-4 text-xl">
-		$page.status
+		{$page.status}
 		{#if $page.status === 404}
 			We checked everywhere, but the thing you wanted was NotFound. Sorry!
 		{:else}An unexpected error occurred.{/if}
