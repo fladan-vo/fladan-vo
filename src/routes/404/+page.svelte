2ea434975d6ebf7cd98ceeaeedc6d69c792331d4
adam <adamliliemark@gmail.com>
2023-10-06 15:42:05 +0200

    different 404

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { base } from '$app/paths'
+</script>
+
+<!-- This page handles any error encountered by the site. -->
+
+<p>
+	<strong>Sorry!</strong>
+	Maybe try one of these links?
+</p>
+<ul>
+	<li>
+		<a href="{base}/">Home</a>
+	</li>
+</ul>
